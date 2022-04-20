9fbd9f0b1674adf60ba5685d82e2e595c75650fd
ihsan.satriawan <ihsan.satriawan@BA-00021650.local>
2022-04-20 07:46:57 +0700

    Change wording

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reac sesit
         </a>
         <Counter />
         <br />
